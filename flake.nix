0d3152967d7e4c7fa7dfcd47a7e895cb3c932a58
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-01-24 14:11:38 +0100

    initial nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "p(rivate)HTTP library dev shell";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+
+
+}
---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  linuxPkgs = with pkgs; lib.optional stdenv.isLinux (
+    inotify-tools
+  );
+  macosPkgs = with pkgs; lib.optional stdenv.isDarwin (
+    with darwin.apple_sdk.frameworks; [
+      CoreFoundation
+      CoreServices
+    ]
+  );
+in
+with pkgs;
+mkShell {
+  nativeBuildInputs = [
+    nodejs-18_x
+    (yarn.override { nodejs = nodejs-18_x; })
+
+    # custom pkg groups
+    linuxPkgs
+    macosPkgs
+  ];
+}
