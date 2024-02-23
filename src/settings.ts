d9517a0dff54981b05ad40e0b34bbb77bd95aa09
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-02-23 17:24:49 +0100

    WIP preparing endpoint

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,5 @@
-// default abort timeout for fetch operations from Discovery Platform
-export const DefaultDPtimeout = 30000;
+// default abort timeout for fetch operations from discovery platform
+export const DefaultDPtimeout = 30_000; // milliseconds
+
+// default abort timeout for unknown endpoint operations
+export const DefaultEndpointTimeout = 60_000; // milliseconds
