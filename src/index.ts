0ccd648ca219db1d9e6cedfccb9d1e674860a088
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-02-23 09:25:33 +0100

    added DP-api module

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export * as DPapi from './dp-api';
 export * as Payload from './payload';
 export * as Result from './result';
---
+++ src/settings.ts
@@ -0,0 +1,2 @@
+// default abort timeout for fetch operations from Discovery Platform
+export const DefaultDPtimeout = 30000;
