15ccad4ac5c460c09fbf40174f1daae57ec1f837
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-03-18 14:18:50 +0100

    renamed to correct stats name

--- src/response.ts
+++ src/response.ts
@@ -9,14 +9,14 @@ import * as Utils from './utils';
  * Stats alongside Response.
  * segDur - duration of first segment http call started until last segment http call finished.
  * rpcDur - duration of RPC request from exit node
- * exitNodeDur - approximate execution duration up to encrypting and compressing response itself
+ * exitAppDur - approximate execution duration up to encrypting and compressing response itself
  * hoprDur - estimated duration of segments during request - response cycle inside hopr network
  */
 export type Stats =
     | {
           segDur: number;
           rpcDur: number;
-          exitNodeDur: number;
+          exitAppDur: number;
           hoprDur: number;
       }
     | { segDur: number };
