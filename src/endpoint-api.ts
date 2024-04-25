683904a2fc508c63d08136f9b6c116de9e633ca2
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-04-25 14:35:35 +0200

    better response props handling

--- src/endpoint-api.ts
+++ src/endpoint-api.ts
@@ -12,28 +12,27 @@ export type Parameters = {
 };
 
 export type Response = {
-    status: number;
-    statusText?: string;
-    text: string;
     headers: Record<string, string>;
+    status: number;
+    statusText: string;
+    text: string;
 };
 
 export async function fetchUrl(endpoint: string, params?: Parameters): Promise<Response> {
-    return new Promise((resolve, reject) => {
-        const url = new URL(endpoint);
-        const headers = params?.headers;
-        const body = params?.body;
-        const method = sanitizeMethod(params?.method);
-        const timeout = params?.timeout ?? DefaultEndpointTimeout;
-        return fetch(url, { headers, method, body, signal: AbortSignal.timeout(timeout) })
-            .then(async (res) => {
-                const status = res.status;
-                const headers = convertRespHeaders(res.headers);
-                const text = await res.text();
-                return resolve({ status, text, headers });
-            })
-            .catch(reject);
-    });
+    const url = new URL(endpoint);
+    const headers = params?.headers;
+    const body = params?.body;
+    const method = sanitizeMethod(params?.method);
+    const timeout = params?.timeout ?? DefaultEndpointTimeout;
+    return fetch(url, { headers, method, body, signal: AbortSignal.timeout(timeout) }).then(
+        async (res) => {
+            const status = res.status;
+            const statusText = res.statusText;
+            const headers = convertRespHeaders(res.headers);
+            const text = await res.text();
+            return { status, statusText, text, headers };
+        },
+    );
 }
 
 function convertRespHeaders(headers: Headers): Record<string, string> {
