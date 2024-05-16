7c07381792c8f6f06137ed6f85f14efb5b6a8bdb
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-05-16 13:48:59 +0200

    first channel open

--- scripts/open-channels.js
+++ scripts/open-channels.js
@@ -1,25 +1,48 @@
 #!/usr/bin/env node
 const { api } = require('@hoprnet/hopr-sdk');
 
-const apiEndpoints = process.env.HOPRD_ENDPOINTS;
+const rawApiEndpoints = process.env.HOPRD_ENDPOINTS;
 const apiToken = process.env.HOPRD_ACCESS_TOKEN;
 
-if (!apiEndpoints) {
+if (!rawApiEndpoints) {
     throw new Error("Missing 'HOPRD_ENDPOINTS' env var");
 }
 if (!apiToken) {
     throw new Error("Missing 'HOPRD_ACCESS_TOKEN' env var");
 }
 
-const payload = { apiEndpoint, apiToken };
+const channelFunding = 1e3; // 1e6;
+const apiEndpoints = rawApiEndpoints.split(',');
 
 async function getAddresses(apiEndpoint) {
-    const addresses = await api.getAddresses({ apiEndpoint, apiToken });
-    return addresses;
+    const res = await api.getAddresses({ apiEndpoint, apiToken });
+    return { apiEndpoint, ...res };
+}
+
+function routes(addresses) {
+    return addresses.reduce((acc, origin) => {
+        const targets = addresses.filter(({ hopr }) => origin.hopr !== hopr);
+        const tRoutes = targets.map((target) => ({ from: origin, to: target }));
+        return acc.concat(tRoutes);
+    }, []);
+}
+
+async function openChannel({ from, to }) {
+    return await api.openChannel({
+        apiEndpoint: from.apiEndpoint,
+        apiToken,
+        amount: channelFunding,
+        peerAddress: to.native,
+    });
 }
 
 async function run() {
     const pAddresses = apiEndpoints.map(getAddresses);
+    const addresses = await Promise.all(pAddresses);
+    const paths = routes(addresses);
+    console.log(`opening ${paths.length} channels on ${addresses.length} nodes`);
+    const pRes = paths.map(openChannel);
+    const results = await Promise.all(results);
 }
 
 run();
