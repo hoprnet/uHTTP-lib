53a71767982f2d877505f11df449155c84100e0a
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-06-11 14:36:23 +0200

    fix relay version

--- src/routing/node-pair.ts
+++ src/routing/node-pair.ts
@@ -18,7 +18,7 @@ export type MessageListener = (messages: NodeAPI.Message[]) => void;
 const MessagesFetchInterval = 333; // ms
 const InfoResponseTimeout = 10e3; // 10s
 
-const RelayNodesCompatVersions = ['2.1.0-rc.4-commit.c2b413c0'];
+const RelayNodesCompatVersions = ['2.1.0'];
 
 export type NodePair = {
     entryNode: EntryNode.EntryNode;
