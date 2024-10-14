43cad9952d27d557b624b4397f2ef2324e1aa9d1
Ronny Esterluss <ronny.esterluss@hoprnet.org>
2024-10-14 16:07:40 +0200

    added segment cache timer

--- src/segment-cache.ts
+++ src/segment-cache.ts
@@ -1,15 +1,27 @@
 import type { Segment } from './segment';
 
-export type Cache = Map<string, Entry>; // requestId -> segmentNr -> Segment
-
-export type Entry = {
-    segments: Map<number, Segment>;
-    count: number; // count segments manually to avoid iterating whole map
+export type Cache = {
+    cache: Map<string, Entry>; // requestId -> Entry
+    remainingSegmentReminder: RemainingSegmentReminder;
 };
 
-export function init(): Cache {
-    return new Map();
+export type RemainingSegmentReminder = (requestId: string, segmentNrs: number[]) => void;
+
+export type Entry = {
+    requestId: string; // requestId for easy reference
+    segments: Map<number, Segment>; // segmentNr -> Segment
+    count: number; // count segments manually to avoid iterating whole map
+    reminder?: ReturnType<typeof setTimeout>; // timeout reminder for missing segments
+};
+
+export function init(remainingSegmentReminder: RemainingSegmentReminder): Cache {
+    return {
+        cache: new Map(),
+        remainingSegmentReminder,
+    };
 }
+
+const SegmentReminderTimeout = 222; // make it slightly bigger than the fetch interval so it wont trigger on regular updates
 
 /**
  * Handles incoming segments against the cache.
@@ -33,10 +45,13 @@ export function incoming(cache: Cache, segment: Segment) {
     }
 
     // adding to existing segments
-    if (cache.has(segment.requestId)) {
-        const entry = cache.get(segment.requestId)!;
-        // do nothing if already cached
+    const requestId = segment.requestId;
+    const cEntry = cache.cache.get(requestId);
+    if (cEntry) {
+        const entry = cEntry as Entry;
+        // do nothing if already cached, except resetting reminder timer
         if (entry.segments.has(segment.nr)) {
+            scheduleReminder(cache, entry);
             return { res: 'already-cached' };
         }
 
@@ -46,19 +61,22 @@ export function incoming(cache: Cache, segment: Segment) {
 
         // check if we are completing
         if (segment.totalCount === entry.count) {
-            cache.delete(segment.requestId);
+            remove(cache, requestId);
             return { res: 'complete', entry };
         }
 
+        scheduleReminder(cache, entry);
         return { res: 'added-to-request', entry };
     }
 
     // creating new entry
     const entry = {
+        requestId,
         segments: new Map([[segment.nr, segment]]),
         count: 1,
     };
-    cache.set(segment.requestId, entry);
+    cache.cache.set(requestId, entry);
+    scheduleReminder(cache, entry);
     return { res: 'inserted-new' };
 }
 
@@ -80,6 +98,45 @@ export function toMessage({ segments, count }: Entry) {
  * Remove everything related to request id.
  *
  */
-export function remove(cache: Cache, requestId: string) {
-    cache.delete(requestId);
+export function remove({ cache }: Cache, requestId: string) {
+    const entry = cache.get(requestId);
+    if (entry) {
+        clearTimeout(entry.reminder);
+        cache.delete(requestId);
+    }
+}
+
+function scheduleReminder(cache: Cache, entry: Entry) {
+    clearTimeout(entry.reminder);
+    entry.reminder = setTimeout(() => remind(cache, entry.requestId), SegmentReminderTimeout);
+}
+
+function remind(cache: Cache, requestId: string) {
+    const cEntry = cache.cache.get(requestId);
+    if (cEntry) {
+        const entry = cEntry as Entry;
+        const missing = missingSegmentNrs(entry);
+        if (missing.length > 0) {
+            scheduleReminder(cache, entry);
+            cache.remainingSegmentReminder(entry.requestId, missing);
+        }
+    }
+}
+
+function segmentCount(entry: Entry) {
+    for (const [, segment] of entry.segments) {
+        return segment.totalCount;
+    }
+    return 0;
+}
+
+function missingSegmentNrs(entry: Entry) {
+    const max = segmentCount(entry);
+    const missing = [];
+    for (let i = 0; i < max; i++) {
+        if (!entry.segments.has(i)) {
+            missing.push(i);
+        }
+    }
+    return missing;
 }
